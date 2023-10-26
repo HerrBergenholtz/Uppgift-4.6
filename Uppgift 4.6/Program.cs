59391029b9cd1a4bb8e5408253fd776336c4d96d
HerrBergenholtz <zbdv8800@gmail.com>
2023-10-26 23:15:02 +0200

    Add project files.

---
+++ Uppgift 4.6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uppgift_4._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett meddelande!");
+            string meddelande = Console.ReadLine();
+
+            Console.WriteLine("Detta meddelande kommer att skrivas ut vertikalt, hur många steg vill du att det ska förskjutas (åt höger");
+            int steg = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                string karaktärer = meddelande[i].ToString();
+
+                for (int j = 0; j < steg; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(karaktärer);
+            }
+            Console.ReadKey();
+        }
+    }
+}
